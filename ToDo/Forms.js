c923b156c4127a203f51ee622f712379d07e6098
MateuszPeliński <pelinski.m@gmail.com>
2018-08-12 15:29:24 +0200

    first commit

---
+++ ToDo/Forms.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Forms = (props) => (
+    <div>List</div>
+)
+
+export default Forms
---
+++ ToDo/ToDo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Forms from './Forms'
+import List from './List'
+
+class Todo extends Component {
+    render() {
+      return (
+        <div>
+            <Forms/>
+            <List/>
+       
+        </div>
+      );
+    }
+  }
+  
+  export default ToDo
